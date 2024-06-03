ac302a3c3ff12376418e82e119ed14111d103312
Boguk <boguk224@gmail.com>
2024-06-03 14:56:06 +0300

    auth-profile

--- frontend/src/screens/Home/main.tsx
+++ frontend/src/screens/Home/main.tsx
@@ -1,27 +1,33 @@
 import ProductList from "../../components/ProductList/main";
 import { WebAppProvider, MainButton } from '@vkruglikov/react-telegram-web-app';
-import {useTelegram} from '../../hooks/useTelegram';
-import {useEffect, useState} from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export const Home = () => {
   const { user, tg } = useTelegram();
-  const userId = user.id; 
+  const userId = user.id;
   const navigate = useNavigate();
   const [profile, setProfile] = useState<any[]>([]);
 
+  console.log(profile)
   useEffect(() => {
     tg.MainButton.setText('Посмотреть корзину');
     tg.MainButton.show();
-  
+
     const onClickHandler = () => {
       navigate(`/cart/${userId}`);
     };
-  
+
     tg.MainButton.onClick(onClickHandler);
 
-    createUser();
+    createOrCheckUser({
+      telegram_id: userId,
+      telegram_name: user.username,
+      chat_id: 1,
+      payment_token: 1
+    });
 
     return () => {
       tg.MainButton.offClick(onClickHandler);
@@ -29,21 +35,23 @@ export const Home = () => {
     };
   }, [tg, navigate, userId, user.id, setProfile]);
 
-  function createUser() {
-    const data = {
-      telegram_id: user.id,
-      telegram_name: user.username,
-      chat_id: 1,
-      payment_token: 1
-    };
-
-    console.table(data);
-
-    return axios.post(`api/v1/profile/${user.id}/create`, data, {
-      headers: {
-        'Content-Type': 'application/json',
+  async function createOrCheckUser(data: any) {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/api/v1/profiles/${userId}`)
+      console.log(response)
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response && err.response.status === 404) {
+        console.log('User not found, creating new profile...');
+        await axios.post(`http://127.0.0.1:8000/api/v1/profile/${userId}/create`, data, {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+        console.log('New user created');
+      } else {
+        console.error('Error creating/updating user:', err);
       }
-    });
+    }
   }
 
   return (
@@ -51,7 +59,7 @@ export const Home = () => {
       <div>
         <ProductList url="http://127.0.0.1:8000/api/v1/products/" />
       </div>
-      <MainButton/>
+      <MainButton />
     </WebAppProvider>
   );
 };
