247d884a6853544d37afb2ca6f9fbe042794cde2
Boguk <boguk224@gmail.com>
2024-06-09 00:17:07 +0300

    add form

--- frontend/src/screens/cart/main.tsx
+++ frontend/src/screens/cart/main.tsx
@@ -1,23 +1,20 @@
 import { useTelegram } from '../../hooks/useTelegram';
 import ProductCartList from '../../components/ProductCartList/main';
 import { WebAppProvider, MainButton,BackButton } from '@vkruglikov/react-telegram-web-app';
-import { useState, useEffect } from 'react';
+import {useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 
 export const Cart = () => {
   const {user, tg} = useTelegram();
   const userId = user.id; 
   const navigate = useNavigate();
-  const [price, setPrice] = useState<any[]>([]);
-
 
   const goBack = () => {
     window.history.back();
   };
   const onClickHandler = () => {
-    navigate(`/form/${userId}/`);
+    navigate(`/cart/form/${userId}/`);
   };
   tg.MainButton.onClick(onClickHandler);
 
@@ -30,20 +27,7 @@ export const Cart = () => {
       console.log(tg.user,tg)
 
     console.log(tg.user,tg)
-    const fetchPrice = async () => {
-      try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/v1/cart/${userId}/price/`);
-        console.log('price',price,setPrice)
-        setPrice(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Ошибка при получении данных:', error);
-      }
-    };
-
-    fetchPrice();
     const url = `http://127.0.0.1:8000/api/v1/cart/${userId}/`;
-    // const url = `http://127.0.0.1:8000/api/v1/cart/123123/`;
 
     return (
       <WebAppProvider>
@@ -57,7 +41,3 @@ export const Cart = () => {
 };
 
 export default Cart
-
-
-
-
