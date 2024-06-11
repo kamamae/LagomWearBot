8d95f318462e5f97072fda95290886fdae57ff49
Boguk <boguk224@gmail.com>
2024-06-11 14:53:44 +0300

    add payment

--- frontend/src/screens/cart/main.tsx
+++ frontend/src/screens/cart/main.tsx
@@ -1,30 +1,49 @@
 import { useTelegram } from '../../hooks/useTelegram';
 import ProductCartList from '../../components/ProductCartList/main';
 import { WebAppProvider, MainButton,BackButton } from '@vkruglikov/react-telegram-web-app';
-import {useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useEffect,useState } from 'react';
+import axios from 'axios';
+
 
 
 export const Cart = () => {
   const {user, tg} = useTelegram();
   const userId = user.id; 
-  const navigate = useNavigate();
+  const [price, setPrice] = useState<number>(0);
+
 
   const goBack = () => {
     window.history.back();
   };
   const onClickHandler = () => {
-    navigate(`/cart/form/${userId}/`);
+    if (price > 0) {
+      tg.sendData(JSON.stringify({ price }));
+    } else {
+      console.error('Invalid price value:', price);
+    }
   };
+
   tg.MainButton.onClick(onClickHandler);
+
+  useEffect(() => {
+    const fetchPrice = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/v1/cart/${userId}/price/`);
+        setPrice(response.data.total_price);
+      } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+      }
+    };
+    fetchPrice();
+  }, [userId]);
 
       useEffect(() => {
   
       tg.MainButton.setParams({
-      text: 'Оформить заказ',
-        });
+        text: `Оплатить ${price} руб.`,
+      });
       }, [tg.user, tg.MainButton, userId]);
-      console.log(tg.user,tg)
+      console.log(tg.user,tg,price)
 
     console.log(tg.user,tg)
     const url = `http://127.0.0.1:8000/api/v1/cart/${userId}/`;
