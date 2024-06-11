6eb2a734dbd0e22b7fb9f6caf19a8fa3823784d2
Boguk <boguk224@gmail.com>
2024-06-11 15:45:27 +0300

    add postEvent

--- frontend/src/screens/cart/main.tsx
+++ frontend/src/screens/cart/main.tsx
@@ -1,21 +1,25 @@
 import { useTelegram } from '../../hooks/useTelegram';
 import ProductCartList from '../../components/ProductCartList/main';
-import { WebAppProvider, MainButton,BackButton } from '@vkruglikov/react-telegram-web-app';
-import {useEffect,useState } from 'react';
+import { WebAppProvider, MainButton, BackButton } from '@vkruglikov/react-telegram-web-app';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
-
 export const Cart = () => {
-  const {user, tg} = useTelegram();
-  const userId = user.id; 
+  const { user, tg } = useTelegram();
+  const userId = user.id;
   const [price, setPrice] = useState<number>(0);
-
 
   const goBack = () => {
     window.history.back();
   };
+
   const onClickHandler = () => {
+    tg.WebApps.postEvent({
+      eventType: 'user_input_received',
+      eventData: {
+        text: 'Hello, Telegram!'
+      }
+    });
     if (price > 0) {
       tg.sendData(JSON.stringify({ price }));
     } else {
@@ -25,38 +29,37 @@ export const Cart = () => {
 
   tg.MainButton.onClick(onClickHandler);
 
+  const updateMainButtonText = () => {
+    tg.MainButton.setParams({
+      text: `Оплатить ${price} руб.`,
+    });
+  };
+
   useEffect(() => {
     const fetchPrice = async () => {
       try {
         const response = await axios.get(`http://127.0.0.1:8000/api/v1/cart/${userId}/price/`);
+        console.log('PRICE', price)
         setPrice(response.data.total_price);
+        updateMainButtonText();
       } catch (error) {
         console.error('Ошибка при получении данных:', error);
       }
     };
     fetchPrice();
-  }, [userId]);
+  }, [userId, price]);
 
-      useEffect(() => {
-  
-      tg.MainButton.setParams({
-        text: `Оплатить ${price} руб.`,
-      });
-      }, [tg.user, tg.MainButton, userId]);
-      console.log(tg.user,tg,price)
+  const url = `http://127.0.0.1:8000/api/v1/cart/${userId}/`;
 
-    console.log(tg.user,tg)
-    const url = `http://127.0.0.1:8000/api/v1/cart/${userId}/`;
-
-    return (
-      <WebAppProvider>
-      <BackButton onClick={goBack}/>
+  return (
+    <WebAppProvider>
+      <BackButton onClick={goBack} />
       <div>
         <ProductCartList url={url} />
       </div>
-      <MainButton  color="#CC87FE"/>
+      <MainButton color="#CC87FE" />
     </WebAppProvider>
   );
 };
 
-export default Cart
+export default Cart;
